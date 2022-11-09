235681b2c5cb8ba7d18f139ee5717a3779b82198
dzaky1201 <dzakyhdr@gmail.com>
2022-11-09 21:22:43 +0700

    get campaign detail endpoint

--- campaign/repository.go
+++ campaign/repository.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type Repository interface {
 	FindAll() ([]Campaign, error)
 	FindByUserID(userId int) ([]Campaign, error)
+	FindByID(ID int) (Campaign, error)
 }
 
 type repository struct {
@@ -15,7 +16,7 @@ func NewRepository(db *gorm.DB) *repository {
 	return &repository{db}
 }
 
-func (r repository) FindAll() ([]Campaign, error) {
+func (r *repository) FindAll() ([]Campaign, error) {
 	var campaigns []Campaign
 	err := r.db.Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
 	if err != nil {
@@ -25,11 +26,22 @@ func (r repository) FindAll() ([]Campaign, error) {
 
 }
 
-func (r repository) FindByUserID(userId int) ([]Campaign, error) {
+func (r *repository) FindByUserID(userId int) ([]Campaign, error) {
 	var campaigns []Campaign
 	err := r.db.Where("user_id = ?", userId).Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
 	if err != nil {
 		return campaigns, err
 	}
 	return campaigns, nil
+}
+
+func (r *repository) FindByID(ID int) (Campaign, error) {
+	var campaign Campaign
+	err := r.db.Preload("User").Preload("CampaignImages").Where("id = ?", ID).Find(&campaign).Error
+
+	if err != nil {
+		return campaign, err
+	}
+
+	return campaign, nil
 }
